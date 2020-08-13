3904fecee6f35145ae3e62354685288a693894d2
Kurt Cooney <KCooney@interdev.com>
2020-08-13 09:04:28 -0400

    First batch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CountDowns
+To get a countdown to a specific date add a query string to the url
+
+The format for the query string ?m=[[MONTH NUMBER]]&d=[[DATE NUMBER]]&y=[[FULL YEAR]]&h=[[HOUR MILITARY TIME]]&mi=[[MINUTES NUMBER]]
--- cnt.js
+++ cnt.js
@@ -1,1 +1,25 @@
-alert('sss')
+const urlParams = new URLSearchParams(window.location.search);
+const offsetHour = parseInt(urlParams.get('h'));
+const theDays = document.getElementById('days');
+const theHrs = document.getElementById('hrs');
+const theMin = document.getElementById('min');
+const theSec = document.getElementById('sec');
+
+const setNumbers = arg => {
+    return arg.toString().length === 1 ? "0"+arg : arg;
+}
+
+
+const a = moment([urlParams.get('y'), urlParams.get('m')-1, urlParams.get('d'), offsetHour, urlParams.get('mi'), 0, 0]);
+let b = moment();
+setInterval(function(){
+    b = moment();
+    let days = a.diff(b, 'days'); 
+    let hrs = ( a.diff(b, 'hours')%24); 
+    let mins = (a.diff(b, 'minutes')%60);    
+    let secs = a.diff(b, 'seconds')%60;
+    theDays.innerHTML = setNumbers(days);
+    theHrs.innerHTML = setNumbers(hrs);
+    theMin.innerHTML = setNumbers(mins);
+    theSec.innerHTML = setNumbers(secs);
+}, 1000);
